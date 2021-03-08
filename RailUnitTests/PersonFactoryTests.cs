82eac79027893b068b6d6dd51704d1a2cd7ce71d
Bo Grandadam <grandadambo@gmail.com>
2021-03-08 21:51:52 +0100

    Made test classes and tests, to test our code.

---
+++ RailUnitTests/PersonFactoryTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RailRoadSimulator;
+
+namespace RailUnitTests
+{
+	[TestClass]
+	public class PersonFactoryTests
+	{
+        [TestMethod]
+        public void DoesFactoryReturnMaidsAndEntity()
+        {
+            //arrange
+            EntityFactory personFactory = new EntityFactory();
+            TempIdentity tempMaid = new TempIdentity();
+            TempIdentity tempEntity = new TempIdentity();
+            string resultMaid;
+            string resultCustomer;
+
+            //act
+            IEntity maid = personFactory.GetPerson("Maid", tempMaid);
+            IEntity person = personFactory.GetPerson("Person", tempEntity);
+            Type typeMaid = maid.GetType();
+            Type typeCostumer = person.GetType();
+            resultMaid = typeMaid.Name.ToString();
+            resultCustomer = typeCostumer.Name.ToString();
+
+            //assert
+            Assert.AreEqual("Maid", resultMaid);
+            Assert.AreEqual("Person", resultCustomer);
+        }
+    }
+}
